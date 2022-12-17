728b57d4ff2a6b52ad00c913a2ffbc2796973524
katiiisha <mendeleevakate2002@gmail.com>
2022-12-17 19:02:31 +0300

    занятие закончилось

---
+++ code.js
@@ -0,0 +1,19 @@
+
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+// console.log(matrix[0][2])
+// const arr = [11, 22, 33, 44, 55, 66, 77, 'kjlkjl']
+// const arr2 = ["иван", "степан", "генадий", "василий", "надежда",]
+// let removed = arr2.splice(1,2)
+// console.log(removed);
+//
+// let arr3 = ['я', 'буду', 'очень', 'плохим', 'футболистом', 'в', 'мире',]
+// let removed1 = arr3.splice(3, 2, 'хорошим', 'программистом',);
+// console.log(arr3);
+
+let arr = [11, 22, 2233, 33, 3344, 44, 22, 4455, 55, 66, 77,]
+
+console.log(arr.includes(33));
